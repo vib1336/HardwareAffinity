514929439e5750f3d101d535a07f78138f1c149e
vib1336 <vib1336@gmail.com>
2020-11-17 21:40:23 +0200

    Scaffold identity system. Add admin account.

---
+++ src/Data/HardwareAffinity.Data/Seeding/UserRolesSeeder.cs
@@ -0,0 +1,42 @@
+﻿namespace HardwareAffinity.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using HardwareAffinity.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using static HardwareAffinity.Common.GlobalConstants;
+
+    public class UserRolesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+            var user = await userManager.FindByNameAsync(AdministratorUserName);
+            var role = await roleManager.FindByNameAsync(AdministratorRoleName);
+
+            if (user == null)
+            {
+                return;
+            }
+
+            var exists = dbContext.UserRoles.Any(ur => ur.UserId == user.Id && ur.RoleId == role.Id);
+
+            if (exists)
+            {
+                return;
+            }
+
+            await dbContext.UserRoles.AddAsync(new IdentityUserRole<string>
+            {
+                UserId = user.Id,
+                RoleId = role.Id,
+            });
+        }
+    }
+}
--- src/HardwareAffinity.Common/GlobalConstants.cs
+++ src/HardwareAffinity.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "HardwareAffinity";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string AdministratorUserName = "Valentin";
     }
 }
